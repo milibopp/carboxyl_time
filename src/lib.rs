5e624ead900daaa9807febc2b485494d0d7c8e65
Emilia Bopp <contact@ebopp.de>
2015-05-21 15:39:36 +0200

    Use time crate for a more informative type
    Also distinguish between UTC and local time.

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,55 @@
 #[macro_use(lift)]
 extern crate carboxyl;
-extern crate clock_ticks;
+extern crate time;
 #[macro_use(lazy_static)]
 extern crate lazy_static;
 
 use carboxyl::Signal;
-use clock_ticks::precise_time_ns;
+use time::Tm;
 
 
-/// Return a signal representing the system time.
-pub fn time() -> Signal<u64> {
-    // Define time signal statically, so there is only ever one unique time
-    // signal. This is necessary to ensure that the system time sampled from
-    // this is always the same within a single transaction.
-    lazy_static! {
-        static ref TIME_SIGNAL: Signal<u64> = lift!(precise_time_ns);
-    }
-    // Make a clone of the static signal.
-    TIME_SIGNAL.clone()
+/// A macro for creating functions that return a handle to a static signal.
+macro_rules! static_signal {
+    ($t: ty, $f: expr) => { {
+        // Define time signal statically, so there is only ever one unique time
+        // signal. This is necessary to ensure that the system time sampled from
+        // this is always the same within a single transaction.
+        lazy_static! {
+            static ref TIME_SIGNAL: Signal<$t> = lift!($f);
+        }
+        // Make a clone of the static signal.
+        TIME_SIGNAL.clone()
+    } }
+}
+
+
+/// A signal of the current local time.
+pub fn now() -> Signal<Tm> {
+    static_signal!(Tm, time::now)
+}
+
+/// A signal of the current UTC time.
+pub fn now_utc() -> Signal<Tm> {
+    static_signal!(Tm, time::now_utc)
 }
 
 
 #[cfg(test)]
 mod test {
     use std::thread;
-    use carboxyl::Sink;
+    use std::fmt::Debug;
+    use carboxyl::{ Signal, Sink };
+    use time::{ Duration, Tm };
 
-    use super::time;
+    use super::{ now, now_utc };
 
 
-    #[test]
-    fn time_samples_equal() {
+    fn samples_equal<A, F: Fn() -> Signal<A>>(f: F)
+        where A: PartialEq + Debug + Send + Clone + Sync + 'static,
+    {
         let sink = Sink::new();
-        let cmp = time().snapshot(
-            &time().snapshot(&sink.stream(), |t, ()| t),
+        let cmp = f().snapshot(
+            &f().snapshot(&sink.stream(), |t, ()| t),
             |t0, t1| (t0, t1)
         );
         let mut events = cmp.events();
@@ -43,12 +59,31 @@ mod test {
     }
 
     #[test]
-    fn time_consistent_with_sleep_ms() {
-        let t0 = time().sample();
+    fn now_samples_equal() {
+        samples_equal(now);
+    }
+
+    #[test]
+    fn now_utc_samples_equal() {
+        samples_equal(now_utc);
+    }
+
+    fn consistent_with_sleep_ms<F: Fn() -> Signal<Tm>>(f: F) {
+        let t0 = f().sample();
         for n in 0..5 {
             thread::sleep_ms(n);
-            let dt = time().sample() - t0;
-            assert!(dt > n as u64 * 1_000_000);
+            let dt = f().sample() - t0;
+            assert!(dt > Duration::milliseconds(n as i64));
         }
+    }
+
+    #[test]
+    fn now_consistent_with_sleep_ms() {
+        consistent_with_sleep_ms(now);
+    }
+
+    #[test]
+    fn now_utc_consistent_with_sleep_ms() {
+        consistent_with_sleep_ms(now_utc);
     }
 }
